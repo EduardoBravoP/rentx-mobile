29708873c81f092d83e73144000ae11c18458410
Eduardo Bravo <eduardobravopires@gmail.com>
2021-09-21 19:37:43 -0300

    created CarCard

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,8 +3,19 @@ import { StatusBar } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 
 import Logo from "../../assets/logo.svg";
+import { Car } from "../../components/Car";
 
-import { Container, Header, TotalCars, HeaderContent } from "./styles";
+import { Container, Header, TotalCars, HeaderContent, CarList } from "./styles";
+
+const carData1 = {
+  brand: "Audi",
+  name: "RS 5 Coupé",
+  rent: {
+    period: "AO DIA",
+    price: 120,
+  },
+  thumbnail: "https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png",
+};
 
 export function Home() {
   return (
@@ -21,6 +32,12 @@ export function Home() {
           <TotalCars>12 carros</TotalCars>
         </HeaderContent>
       </Header>
+
+      <CarList
+        data={[1, 2, 3, 4, 5, 7]}
+        renderItem={({ item }) => <Car data={carData1} />}
+        keyExtractor={(item) => String(item)}
+      />
     </Container>
   );
 }
